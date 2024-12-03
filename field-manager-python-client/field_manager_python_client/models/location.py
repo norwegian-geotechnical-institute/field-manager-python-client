e6a89ba39d1926ba2388f8c9fc334ae8f027b760
yejiyang <6262071+yejiyang@users.noreply.github.com>
2024-12-03 00:15:01 +0000

    [create-pull-request] automated change

--- field-manager-python-client/field_manager_python_client/models/location.py
+++ field-manager-python-client/field_manager_python_client/models/location.py
@@ -45,8 +45,8 @@ T = TypeVar("T", bound="Location")
 class Location:
     """
     Example:
-        {'location_id': '0e6343a6-159c-43fa-8893-767fbdfa7ccf', 'name': 'Loc01', 'point_easting': 1194547,
-            'point_northing': 8388298, 'point_z': 0.0, 'project_id': '09198cee-6bc6-49f8-b499-2cce0cd41716', 'srid': 3857}
+        {'location_id': 'ccde6673-f4ec-4403-a9de-874388d17e52', 'name': 'Loc01', 'point_easting': 1194547,
+            'point_northing': 8388298, 'point_z': 0.0, 'project_id': 'e277db66-f3b5-41ec-9b2d-c9715303f2e2', 'srid': 3857}
 
     Attributes:
         name (str):
