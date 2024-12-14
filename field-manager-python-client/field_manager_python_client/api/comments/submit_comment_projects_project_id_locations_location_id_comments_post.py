814b0c3960de4af0eab3e20c84221be6e5030a8b
yejiyang <6262071+yejiyang@users.noreply.github.com>
2024-12-14 00:13:32 +0000

    [create-pull-request] automated change

--- field-manager-python-client/field_manager_python_client/api/comments/submit_comment_projects_project_id_locations_location_id_comments_post.py
+++ field-manager-python-client/field_manager_python_client/api/comments/submit_comment_projects_project_id_locations_location_id_comments_post.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-from typing import Any, Dict, Optional, Union
+from typing import Any, Optional, Union
 from uuid import UUID
 
 import httpx
@@ -18,10 +18,10 @@ def _get_kwargs(
     *,
     body: CommentCreate,
     method_id: Union[None, UUID, Unset] = UNSET,
-) -> Dict[str, Any]:
-    headers: Dict[str, Any] = {}
+) -> dict[str, Any]:
+    headers: dict[str, Any] = {}
 
-    params: Dict[str, Any] = {}
+    params: dict[str, Any] = {}
 
     json_method_id: Union[None, Unset, str]
     if isinstance(method_id, Unset):
@@ -34,7 +34,7 @@ def _get_kwargs(
 
     params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
 
-    _kwargs: Dict[str, Any] = {
+    _kwargs: dict[str, Any] = {
         "method": "post",
         "url": f"/projects/{project_id}/locations/{location_id}/comments",
         "params": params,
