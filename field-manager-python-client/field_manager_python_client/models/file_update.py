29dbb52247b4400846c1175f8b3211077591308d
yejiyang <6262071+yejiyang@users.noreply.github.com>
2025-01-05 00:14:15 +0000

    [create-pull-request] automated change

--- field-manager-python-client/field_manager_python_client/models/file_update.py
+++ field-manager-python-client/field_manager_python_client/models/file_update.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Type, TypeVar, Union, cast
+from typing import Any, TypeVar, Union, cast
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
@@ -18,9 +18,9 @@ class FileUpdate:
 
     name: Union[None, Unset, str] = UNSET
     comment: Union[None, Unset, str] = UNSET
-    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
-    def to_dict(self) -> Dict[str, Any]:
+    def to_dict(self) -> dict[str, Any]:
         name: Union[None, Unset, str]
         if isinstance(self.name, Unset):
             name = UNSET
@@ -33,7 +33,7 @@ class FileUpdate:
         else:
             comment = self.comment
 
-        field_dict: Dict[str, Any] = {}
+        field_dict: dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update({})
         if name is not UNSET:
@@ -44,7 +44,7 @@ class FileUpdate:
         return field_dict
 
     @classmethod
-    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
         d = src_dict.copy()
 
         def _parse_name(data: object) -> Union[None, Unset, str]:
@@ -74,7 +74,7 @@ class FileUpdate:
         return file_update
 
     @property
-    def additional_keys(self) -> List[str]:
+    def additional_keys(self) -> list[str]:
         return list(self.additional_properties.keys())
 
     def __getitem__(self, key: str) -> Any:
