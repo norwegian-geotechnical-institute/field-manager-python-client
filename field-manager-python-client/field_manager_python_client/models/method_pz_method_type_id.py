4e8f7e902b9b59e8a05de0e51db688896c608768
yejiyang <6262071+yejiyang@users.noreply.github.com>
2025-01-27 00:13:23 +0000

    [create-pull-request] automated change

--- field-manager-python-client/field_manager_python_client/models/method_pz_method_type_id.py
+++
@@ -1,8 +0,0 @@
-from enum import IntEnum
-
-
-class MethodPZMethodTypeId(IntEnum):
-    VALUE_5 = 5
-
-    def __str__(self) -> str:
-        return str(self.value)
--- field-manager-python-client/field_manager_python_client/models/method_tot_update_method_type_id.py
+++
@@ -1,8 +0,0 @@
-from enum import IntEnum
-
-
-class MethodTOTUpdateMethodTypeId(IntEnum):
-    VALUE_2 = 2
-
-    def __str__(self) -> str:
-        return str(self.value)
