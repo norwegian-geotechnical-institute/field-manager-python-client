bc6e8607aace83b75e889e30e28f171cc099ddc0
Jiyang Ye <jiyang.ye@ngi.no>
2025-03-13 14:48:33 +0100

    [create-pull-request] automated change (#91)
    Co-authored-by: yejiyang <6262071+yejiyang@users.noreply.github.com>

--- field-manager-python-client/field_manager_python_client/models/create_cross_section_lines_projects_project_id_cross_sections_lines_format_post_format.py
+++ field-manager-python-client/field_manager_python_client/models/create_cross_section_lines_projects_project_id_cross_sections_lines_format_post_format.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class CreateCrossSectionLinesProjectsProjectIdCrossSectionsLinesFormatPostFormat(str, Enum):
     DXF = "dxf"
-    SHP = "shp"
+    ZIP = "zip"
 
     def __str__(self) -> str:
         return str(self.value)
